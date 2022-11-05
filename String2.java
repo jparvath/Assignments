6308c332d8d9e01da86da760fcb9da887810af5b
jparvath <115786149+jparvath@users.noreply.github.com>
2022-11-05 23:12:26 +0530

    String2.java

---
+++ String2.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.*;
+ class Sting2
+{
+    public static void main(String[] args)
+    {
+        String a = "Hello";
+        String b = " how are you?";
+        String c = a.concat(b);
+        System.out.println(c);
+    }
+}
