cd66fba7c0886fffcfe08b2034aa32e3c94548f7
jparvath <115786149+jparvath@users.noreply.github.com>
2022-11-06 11:28:56 +0530

    ProductColl.java

---
+++ ProductColl.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+import java.util.*;
+
+public class ProductColl {
+    public static void main(String[] args)
+    {
+        HashSet<String> obj=new HashSet<>();
+        obj.add("Glasses");
+        obj.add("chairs");
+        obj.add("pens");
+        obj.add("table");
+        obj.add("Books");
+        obj.add("papers");
+        obj.add("sketchers");
+        obj.add("spoons");
+        obj.add("colors");
+        obj.add("mobiles");
+        System.out.println(obj);
+    }
+}
+
+
