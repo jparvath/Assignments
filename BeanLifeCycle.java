63666c9132bdbde3529747c7f742a42a8e570817
jparvath <115786149+jparvath@users.noreply.github.com>
2022-11-20 22:21:03 +0530

    BeanLifeCycle.java

---
+++ BeanLifeCycle.java
@@ -0,0 +1,30 @@
+package SpringA1;
+
+/**
+ * TODO jparvath This type ...
+ *
+ */
+public class BeanLifeCycle {
+
+  private String message;
+
+  public void setMessage(String message) {
+
+    this.message = message;
+  }
+
+  public void getMessage() {
+
+    System.out.println("Your Message : " + this.message);
+  }
+
+  public void init() {
+
+    System.out.println("Bean is going through init.");
+  }
+
+  public void destroy() {
+
+    System.out.println("Bean will destroy now.");
+  }
+}
