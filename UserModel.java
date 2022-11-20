c1ea60636a8275b25ed14a854d7a6c5a9c42268c
jparvath <115786149+jparvath@users.noreply.github.com>
2022-11-20 22:27:54 +0530

    UserModel.java

---
+++ UserModel.java
@@ -0,0 +1,29 @@
+package webapp;
+
+public class UserModel {
+
+  private String Username;
+
+  private String Password;
+
+  public String getUsername() {
+
+    return this.Username;
+  }
+
+  public void setUsername(String username) {
+
+    this.Username = username;
+  }
+
+  public String getPassword() {
+
+    return this.Password;
+  }
+
+  public void setPassword(String password) {
+
+    this.Password = password;
+  }
+
+}
