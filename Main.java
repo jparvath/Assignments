3b61d4290e83ab7094e09a3d832f5e3d2c635a67
jparvath <115786149+jparvath@users.noreply.github.com>
2022-11-20 22:19:45 +0530

    Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,26 @@
-package com.company;
+package springcore_example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * TODO jparvath This type ...
+ *
+ */
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("hello world");
+  private static AbstractApplicationContext context1;
 
-    }
-}
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+
+  }
+   ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+   HelloBean helloBean = (HelloBean) context1.getBean("HelloBean");
+
+   hellobean.sayHello();
+  }
