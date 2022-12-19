4a081fcacc906ed51ee53602e23cf820027a69c6
jparvath <115786149+jparvath@users.noreply.github.com>
2022-12-19 19:59:16 +0530

    Add files via upload

---
+++ movie-info-service/src/main/java/io/microservice/movieinfoservice/models/Movie.java
@@ -0,0 +1,31 @@
+package io.microservice.movieinfoservice.models;
+
+public class Movie {
+
+	public Movie() {
+
+	}
+
+	private String movieId;
+	private String name;
+	public Movie(String movieId, String name) {
+		super();
+		this.movieId = movieId;
+		this.name = name;
+	}
+	public String getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+}
