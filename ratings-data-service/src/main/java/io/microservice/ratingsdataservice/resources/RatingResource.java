cd7004112c629eed862c6f5d381ebc13a194fbeb
jparvath <115786149+jparvath@users.noreply.github.com>
2022-12-19 20:00:17 +0530

    Add files via upload

---
+++ ratings-data-service/src/main/java/io/microservice/ratingsdataservice/resources/RatingResource.java
@@ -0,0 +1,35 @@
+package io.microservice.ratingsdataservice.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.microservice.ratingsdataservice.models.Rating;
+import io.microservice.ratingsdataservice.models.UserRating;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingResource {
+
+	@RequestMapping("/{movieId}")
+	public Rating getRating(@PathVariable("movieId") String movieId) {
+
+		return new Rating("movieid",4);
+	}
+
+	@RequestMapping("users/{userId}")
+	public UserRating getUserRating(@PathVariable("userId") String userId) {
+		List<Rating> ratings = Arrays.asList(
+				new Rating("1234",4),
+				new Rating("5678",3)
+				);
+		UserRating userRating = new UserRating();
+		userRating.setUserRating(ratings);
+		return userRating;
+
+	}
+
+}
